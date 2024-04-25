3129e71ce5cfd638ddfeedff7b6ed3bf7dfa0945
Abbos29 <abbosvaliev2003@gmail.com>
2024-04-25 22:30:23 +0500

    feat: finish v1

--- script/script.js
+++ script/script.js
@@ -7,3 +7,22 @@ const menu = document.querySelector('.menu')
 burger.addEventListener('click', () => {
     menu.classList.toggle('open')
 })
+
+
+
+
+// INPUT RANGE
+
+const sliderEl = document.querySelector("#range2")
+const sliderValue = document.querySelector(".value2")
+
+sliderEl.addEventListener("input", (event) => {
+    const tempSliderValue = event.target.value;
+    sliderValue.textContent = tempSliderValue;
+
+    const progress = (tempSliderValue / sliderEl.max) * 100;
+
+    // sliderEl.style.background = `linear-gradient(to right, #f50 ${progress}%, #ccc ${progress}%)`;
+    sliderEl.style.background = `#ccc`;
+})
+
